34ae706788db9de47fa90fbc3375dabf5f727a2b
Andy Griffiths <andygriffiths@Andys-MacBook-Pro.local>
2021-05-13 12:07:08 +0100

    added sub title

--- app.js
+++ app.js
@@ -79,7 +79,7 @@
 
         results.innerHTML = `
             <h3 class="text-2xl mb-5">Results</h3>
-            <p>If you acquire <strong class="text-2xl text-green-400">${monthlyCustomers}</strong> customers per month paying <strong class="text-2xl text-green-400">£${monthlySubscriptionPrice}</strong> each - your revenue in the first ${months} months will be <strong class="text-2xl text-green-400">£${thousands_separators(annualTurnover.toFixed(2))}</strong></p>
+            <p>If you acquire <strong class="text-2xl text-green-400">${monthlyCustomers}</strong> customers per month paying <strong class="text-2xl text-green-400">£${monthlySubscriptionPrice}</strong> each - <br class="hidden md:inline"/>your revenue in the first ${months} months will be <strong class="text-2xl text-green-400">£${thousands_separators(annualTurnover.toFixed(2))}</strong></p>
             <div class="flex flex-col md:flex-row">
                 <div class="annual-turnover-result-row mt-10 border-2 border-gray-900 p-2 md:mr-2 shadow flex-grow">
                     <div class="text-lg"><strong>MRR</strong> <br/><span class="text-xs">(Monthly Recurring Revenue)</span> <br/><span class="font-bold text-sm">at month ${months}</span></div>
